9a839a9f98a57ecb99ea8e2099da8a79956f21fa
sarakrkljes <126513575+sarakrkljes@users.noreply.github.com>
2023-08-14 10:00:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly");
+$("#myID").on("click", function(){
+    console.log("You clicked on #myID");
+});
+
+$("myID").on("click", function(){
+    $(".fadeMe").fadeToggle("slow");
+});
